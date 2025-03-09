93885e17839225d370f0ca6d36aac8b827ac0516
HarshaFdo <fernando.harsha2016@gmail.com>
2025-03-09 16:26:13 +0530

    customize the layout

---
+++ app/meals/share/page.js
@@ -0,0 +1,3 @@
+export default function ShareaMealPage() {
+  return <h1>Share Meal</h1>
+}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-import Header from "@/components/header";
-
 export default function Home() {
   return (
-      <main >
-        <Header />
-        <p><Link href="/about">About Us</Link></p>
-      </main>
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meal</Link></p>
+      <p><Link href="/community">Community</Link></p>
+    </main>
   );
 }
-
-
